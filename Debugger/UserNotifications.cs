eb6d2bf3ce11f951db53fffc138525fdeac9f3e7
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-03-28 17:23:43 +0200

    add notification

--- Debugger/UserNotifications.cs
+++ Debugger/UserNotifications.cs
@@ -16,6 +16,7 @@ namespace ModTools
         static UserNotifications()
         {
             Add(LoggingChangeNotification);    
+            Add(UnityLoggingHookNotification);
         }
 
         private static void Add(string notification)
@@ -48,6 +49,10 @@ namespace ModTools
 It offers an improved experience over the old one but requires a change to your logging code.
 You should no longer use DebugOutputPanel for logging as messages sent to it won't get displayed by ModTools.
 Instead you should use the built-in Unity Debug API (http://docs.unity3d.com/ScriptReference/Debug.html). Example: Debug.Log(""Hello world!"");";
+
+        private static string UnityLoggingHookNotification = @"Your version of ModTools has a new feature which allows it to hook Unity's Debug logging so you can safely log from the simulation thread (or any other thread).
+This feature is currently marked as experimental and is off by default. You can find it in the main menu (Ctrl+Q) as ""Hook Unity's logging (experimental)"". After enabling it you should see a warning in the console saying so.
+It is recommended that you enable this (as it will probably become the default mode in the future) and report any issues.";
 
     }
 
